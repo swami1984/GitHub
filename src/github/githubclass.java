801611f43b2f8195d8747e60aef61bed61c6e9e5
swami1984 <86766166+swami1984@users.noreply.github.com>
2022-09-08 14:20:02 +0530

    commit changes to local repo

---
+++ src/github/githubclass.java
@@ -0,0 +1,12 @@
+package github;
+
+public class githubclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("first java project");
+
+	}
+
+}
