b268e96a8313d9925b40bce4fef5b37ffc3f1bae
swami1984 <86766166+swami1984@users.noreply.github.com>
2022-09-08 18:38:44 +0530

    Update githubclass.java

--- src/github/githubclass.java
+++ src/github/githubclass.java
@@ -5,8 +5,8 @@ public class githubclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("first java project");
-		System.out.println("first java project");
+		System.out.println("first java projectss");
+		
 		
 
 	}
