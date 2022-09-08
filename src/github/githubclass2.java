d404820a19cf192e51d57725f385babcb17e818b
swami1984 <86766166+swami1984@users.noreply.github.com>
2022-09-08 14:40:44 +0530

    second file pushing to new branch

---
+++ src/github/githubclass2.java
@@ -0,0 +1,12 @@
+package github;
+
+public class githubclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("first java project2");
+
+	}
+
+}
